46cd4f21b1f33508bdd2eb241ab1e457f6f2122a
Pedro Alisson <p.alissonpd@gmail.com>
2019-01-15 00:17:55 -0300

    Primeiro commit

---
+++ busca.py
@@ -0,0 +1,33 @@
+from classes import No
+from Queue import PriorityQueue
+
+
+def solucao(no):
+    lista = []
+    while no:
+        lista.append(no.estado)
+        no = no.pais
+    lista.reverse()
+    return lista
+
+# Busca em Largura
+def BFS(problema)
+    no = No(problema.inicio)
+    if problema.test_objetivo(no.estado)
+        return [no.estado]
+    borda = [no]
+    explorado = set()
+
+    while borda:
+        no = borda.pop(0)
+        explorado.add(no.estado)
+        for acao in problema.acoes(no.estado)
+            filho = No(acao, no)
+            if filho not in borda and filho.estado not in explorado:
+                if problema.test_objetivo(filho.estado):
+                    return solucao(filho)
+                borda.append(filho)
+    return None
+
+
+
---
+++ classes.py
@@ -0,0 +1,45 @@
+class Problema(object):
+
+    def __init__(self, inicio, objetivo=None, acao=None):
+        self.inicio = inicio
+        self.objetivo = objetivo
+        self.acao = acao
+
+    def test_objetivo(self, estadoNo):
+        return estadoNo == self.objetivo
+
+    def acoes(self,estadoNo):
+        return list(self.acao[estadoNo])
+
+
+class No(object):
+    def __init__(self, estado, pais=None, acao=None, custo_caminho=0, valor=None):
+        self.estado = estado
+        self.pais = pais
+        if pais:
+            self.acao = pais.estado + "->" + estado
+        self.custo_caminho = custo_caminho
+        self.valor = valor
+        self.profundidade = 0
+        if pais:
+            self.profundidade = pais.profundidade
+
+    # def solucao(self):
+    #     return [no.acao for no in self.caminho()[1:]]
+
+    # def caminho(self):
+    #     no, caminho_volta = self, []
+    #     while no:
+    #         caminho_volta.append(no)
+    #         no = no.pais
+    #     return list(reversed(caminho_volta))
+
+   # def no_filho(self, problema, acao):
+      #  prox_estado =
+
+   # def expandir(self, problema):
+
+
+    def __repr__(self):
+        return self.estado + ", " + self.acao
+
