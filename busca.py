892d05377903d9a58e25108acf64fb0e1a3a05af
Pedro Alisson <p.alissonpd@gmail.com>
2019-01-18 10:13:23 -0300

    Adicionando DFS e DFSV

--- busca.py
+++ busca.py
@@ -29,5 +29,45 @@ def BFS(problema):
                 borda.append(filho)
     return None
 
+# Busca em profundidade
+def DFS(problema):
+    no = No(problema.inicio)
+    if problema.test_objetivo(no.estado):
+        return [no.estado]
+    borda = [no]
+
+    while borda:
+        no = borda.pop(-1)
+        for acao in problema.acoes(no.estado)
+            filho = No(acao, no)
+            if filho not in borda:
+                if problema.test_objetivo(filho.estado)
+                    return solucao(filho)
+                borda.append(filho)
+    return None
+
+# Busca em profundidade com lista de visitados
+
+def DFSV(problema):
+    no = No(problema.inicio)
+    if problema.test_objetivo(no.estdo):
+        return [no.estado]
+    borda = []
+    explorado = set()
+    borda.append(no)
+
+    while borda:
+        no = borda.pop(-1)
+        explorado.add(no.estado)
+
+        for acao in problema.acoes(no.estado):
+            filho = No(acao, no)
+
+            if filho.estado not in explorado and filho not in borda:
+                if problema.test_objetivo(filho.estado):
+                    return solucao(filho)
+                if filho.estado not in explorado:
+                    borda.append(filho)
+    return None
 
 
